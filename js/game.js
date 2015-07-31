8dda3a5473211b191d104468bd46989db13f3729
Yaroslav Zaklinskyi <Yaroslav_Zaklinskyi@epam.com>
2015-07-31 12:11:45 +0300

    Base game loop created

---
+++ js/game.js
@@ -0,0 +1,33 @@
+;(function(){
+    var Game = function(canvasId){
+        var canvas = document.getElementById(canvasId);
+        var screen = canvas.getContext("2d");
+        var gameSize = {
+            x: canvas.width,
+            y: canvas.height
+        };
+
+        var self = this;
+        var tick = function(){
+            self.update();
+            self.draw(screen, gameSize);
+            requestAnimationFrame(tick)
+        };
+
+        tick();
+    }
+
+    Game.prototype = {
+        update: function(){
+            console.log("Update");
+        },
+
+        draw: function(screen, gameSize){
+            screen.fillRect(150, 150, 32, 32);
+        }
+    }
+
+    window.onload = function(){
+        new Game("screen");
+    }
+})();
